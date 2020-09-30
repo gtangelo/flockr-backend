ba139a1369a0f90ad3b37ecef7c0b113aa355fac
Gabriel Ting <65492346+gtangelo@users.noreply.github.com>
2020-09-30 20:42:02 +1000

    added data structure and validate funct (copied from channel branch)

---
+++ src/data.py
@@ -0,0 +1,62 @@
+data = {
+    'active_users': [
+        {
+            'token': 0,
+            'u_id': 1,
+            'email': 'cs1531@cse.unsw.edu.au',
+            'name_first': 'Hayden',
+            'name_last': 'Jacobs',
+            'handle_str': 'hjacobs',
+        },
+    ],
+    'users': [
+        {
+            'u_id': 1,
+            'email': 'cs1531@cse.unsw.edu.au',
+            'name_first': 'Hayden',
+            'name_last': 'Jacobs',
+            'handle_str': 'hjacobs',
+        },
+    ],
+    'channels': [
+        {
+            'channel_id': 1,
+            'name' : 'channel1',
+            'total_messages': 3,
+            'messages': [
+                {
+                    'message_id': 1,
+                    'u_id': 1,
+                    'message': 'Hello world',
+                    'time_created': 1582426789,
+                },
+                {
+                    'message_id': 2,
+                    'u_id': 2,
+                    'message': 'Hello user1!',
+                    'time_created': 1582426790,
+                },
+                {
+                    'message_id': 3,
+                    'u_id': 1,
+                    'message': 'Hello user2',
+                    'time_created': 1582426791,
+                },
+            ],
+            'members': [
+                {
+                    'u_id': 1,
+                    'name_first' : 'user1',
+                    'name_last': 'last'
+                },
+            ],
+            'owners': [
+                {
+                    'u_id': 1,
+                    'name_first' : 'user1',
+                    'name_last': 'last'
+                },
+            ],
+        },
+    ],
+}
