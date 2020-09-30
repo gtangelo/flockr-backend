113484e3b55e66129e3362dcbe7e749ab591baff
Christian Ilagan <cmilagan7@gmail.com>
2020-09-30 14:51:16 +1000

    implementation of auth feature, with some changes to be made to testing

--- src/auth.py
+++ src/auth.py
@@ -1,16 +1,39 @@
+"""
+This feature covers the basic process of creating and managing users.
+"""
+
+data = {
+    'users': []
+}
+
 def auth_login(email, password):
-    return {
-        'u_id': 1,
-        'token': '12345',
-    }
+    for user in data['users']:
+        if user['email'] == email:
+            if user['password'] == password:
+                user['online'] = True
+                return True
+    return False
 
 def auth_logout(token):
-    return {
-        'is_success': True,
-    }
+    for user in data['users']:
+        if user['token'] == token:
+            user['online'] == False
+            return True
+    return False
 
 def auth_register(email, password, name_first, name_last):
-    return {
-        'u_id': 1,
-        'token': '12345',
+    for user in data['users']:
+        if user['email'] == email:
+            return False
+    
+    newUser = {
+        'email': email,
+        'password': password,
+        'first_name': name_first,
+        'last_name': name_last,
+        'u_id': len(data['users']) + 1,
+        'token': email,
+        'online': False
     }
+    data['users'].append(newUser)
+    return True
--- src/test_auth.py
+++ src/test_auth.py
@@ -1,3 +1,38 @@
+import auth
+import pytest
 '''
 Tests for auth.py
 '''
+def test_register_twice():
+    auth.auth_register('testEmail@gmail.com', 'abcdefg', 'Christian', 'Ilagan')
+    assert auth.auth_register('testEmail@gmail.com', 'this is', 'already', 'registered') == False
+
+def test_login():
+    auth.auth_register('testEmail@gmail.com', 'abcdefg', 'Christian', 'Ilagan')
+    auth.auth_login('testEmail@gmail.com', 'abcdefg')
+
+def test_login_invalid():
+    auth.auth_register('testEmail@gmail.com', 'abcdefg', 'Christian', 'Ilagan')
+    assert auth.auth_login('ThisShouldNotWork@gmail.com', 'abcdefg') == False
+
+def test_logout():
+    auth.auth_register('testEmail@gmail.com', 'abcdefg', 'Christian', 'Ilagan')
+    auth.auth_login('testEmail@gmail.com', 'abcdefg')
+    assert auth.auth_logout('testEmail@gmail.com') == True
+
+def test_uid():
+    auth.auth_register('testEmail@gmail.com', 'abcdefg', 'Christian', 'Ilagan')
+    for user in auth.data['users']:
+        if user['email'] == 'testEmail@gmail.com':
+            assert user['u_id'] == 1
+    auth.auth_register('blah@gmail.com', 'abcde', 'George', 'Curious')
+    auth.auth_register('blah2@gmail.com', 'abcde', 'George', 'Curious')
+    auth.auth_register('blah3@gmail.com', 'abcde', 'George', 'Curious')
+    for user in auth.data['users']:
+        if user['email'] == 'blah2@gmail.com':
+            assert user['u_id'] == 3
+
+
+    
+
+
