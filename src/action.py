bb86d9f1706cf57ea42efcaa1ba2f4a27bb59bc5
Gabriel Ting <65492346+gtangelo@users.noreply.github.com>
2020-10-13 13:21:42 +1100

    fix coverage where i can

--- src/action.py
+++ src/action.py
@@ -59,10 +59,12 @@ def get_details_from_u_id(u_id):
                     channels, is_flockr_owner}
                 details containing user information
     """
+    details = {}
     for user in data['users']:
         if user['u_id'] == u_id:
-            return user
-    return {}
+            details = user
+            break
+    return details
 
 def add_channel_to_user_list(u_id, channel):
     """Add channel information on the user list in the data structure
@@ -73,20 +75,15 @@ def add_channel_to_user_list(u_id, channel):
                     channels, is_flockr_owner}
                 details containing user information
     """
-    in_channel = False
-    for user_index, user in enumerate(data['users']):
+    for user in data['users']:
         if user['u_id'] == u_id:
             add_channel = {}
             add_channel['channel_id'] = channel['channel_id']
             add_channel['name'] = channel['name']
             add_channel['is_public'] = channel['is_public']
-            for curr_channel in user['channels']:
-                if curr_channel['channel_id'] == channel['channel_id']:
-                    in_channel = True
-                    break
-            if in_channel:
-                data['users'][user_index]['channels'].append(add_channel)
-            return
+            user['channels'].append(add_channel)
+            break
+    return
 
 def get_lowest_u_id_user_in_channel(channel):
     """Return information of user in the channel with the lowest u_id
@@ -101,13 +98,13 @@ def get_lowest_u_id_user_in_channel(channel):
                 details containing user information. Return `None` if there
                 are no members
     """
+    lowest_u_id_user = {}
     if len(channel['all_members']) > 0:
-        lowest_u_id_user = channel['all_members'][0]
+        lowest_u_id_user = data['users'][-1]
         for user in channel['all_members']:
             if lowest_u_id_user['u_id'] > user['u_id']:
                 lowest_u_id_user = user
-        return lowest_u_id_user
-    return None
+    return lowest_u_id_user
 
 def remove_channel_in_user_list(u_id, channel_id):
     """Remove user with u_id in the channel within the database
