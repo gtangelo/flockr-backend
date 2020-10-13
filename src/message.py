e541684a94c1d650ed7e2f68a04de5ce28c8b64b
Prathamesh Jagtap <prathsjagtap@gmail.com>
2020-10-13 18:25:48 +1100

    Completed implementation for message_send

--- src/message.py
+++ src/message.py
@@ -16,6 +16,7 @@ from validate import (
     validate_u_id,
     validate_u_id_as_channel_owner,
     validate_user_as_member,
+    validate_u_id_as_flockr_owner,
 )
 from action import (
     get_details_from_u_id,
@@ -48,7 +49,38 @@ def message_remove(token, message_id):
     Returns:
         (dict): {}
     """
+    # check valid token (AccessError)
+    if not validate_token(token):
+        raise AccessError("Token is invalid, please register/login")
+
+    # check valid message_id (InputError) (each message has a unique id)
+    on_list = False
+    channel_details = {}
+    for channels in data['channels']:
+        for messages in channels['messages']:
+            if messages['message_id'] == message_id:
+                on_list = True
+                channel_details = channels
+    if not on_list:
+        raise InputError("Message does not exist")
     
+    # check if user is authorized
+    authorized = False
+    user_details = convert_token_to_user(token)
+    condition_1 = validate_u_id_as_flockr_owner(user_details['u_id'])
+    condition_2 = validate_u_id_as_channel_owner(user_details['u_id'], channel_details)
+    if condition_1 or condition_2:
+        authorized = True
+    if not authorized:
+        raise AccessError("User not authorized to remove message")
+
+    # remove the message if user is flockr owner or channel owner
+    # (Assumption) flockr owner does not need to be a part of the channel to remove message
+    for channels in data['channels']:
+        for messages in channels['messages']:
+            if messages['message_id'] == message_id:
+                channels['messages'].remove(messages)
+    return {}
 
 def message_edit(token, message_id, message):
     """Given a message, update it's text with new text. If the new message is an
