ff33c5c8e06d18e05157a412abc454f743385926
Gabriel Ting <65492346+gtangelo@users.noreply.github.com>
2020-10-14 22:29:50 +1100

    added setuo files for flask

---
+++ src/other_http_test.py
@@ -0,0 +1,70 @@
+import pytest
+import re
+from subprocess import Popen, PIPE
+import signal
+from time import sleep
+import requests
+import json
+
+
+# Use this fixture to get the URL of the server. It starts the server for you,
+# so you don't need to.
+@pytest.fixture
+def url():
+    url_re = re.compile(r' \* Running on ([^ ]*)')
+    server = Popen(["python3", "src/server.py"], stderr=PIPE, stdout=PIPE)
+    line = server.stderr.readline()
+    local_url = url_re.match(line.decode())
+    if local_url:
+        yield local_url.group(1)
+        # Terminate the server
+        server.send_signal(signal.SIGINT)
+        waited = 0
+        while server.poll() is None and waited < 5:
+            sleep(0.1)
+            waited += 0.1
+        if server.poll() is None:
+            server.kill()
+    else:
+        server.kill()
+        raise Exception("Couldn't get URL from local server")
+
+# Example testing from echo_http_test.py
+# def test_echo(url):
+#     '''
+#     A simple test to check echo
+#     '''
+#     resp = requests.get(url + 'echo', params={'data': 'hello'})
+#     assert json.loads(resp.text) == {'data': 'hello'}
+
+#------------------------------------------------------------------------------#
+#                                   users/all                                  #
+#------------------------------------------------------------------------------#
+
+#?-------------------------- Input/Access Error Testing ----------------------?#
+
+
+#?------------------------------ Output Testing ------------------------------?#
+
+
+
+#------------------------------------------------------------------------------#
+#                          admin/userpermission/change                         #
+#------------------------------------------------------------------------------#
+
+#?-------------------------- Input/Access Error Testing ----------------------?#
+
+
+#?------------------------------ Output Testing ------------------------------?#
+
+
+#------------------------------------------------------------------------------#
+#                                    search                                    #
+#------------------------------------------------------------------------------#
+
+#?-------------------------- Input/Access Error Testing ----------------------?#
+
+
+#?------------------------------ Output Testing ------------------------------?#
+
+
