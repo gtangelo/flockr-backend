4da3bc26b15793cba9c45f4bf6d6114eac13a7e6
Gabriel Ting <65492346+gtangelo@users.noreply.github.com>
2020-10-18 17:40:17 +1100

    channel_leave - started http tests

--- src/channel_http_test.py
+++ src/channel_http_test.py
@@ -1,3 +1,4 @@
+from json import dumps
 import pytest
 import re
 from subprocess import Popen, PIPE
@@ -5,6 +6,8 @@ import signal
 from time import sleep
 import requests
 import json
+
+from error import InputError, AccessError
 
 
 # Use this fixture to get the URL of the server. It starts the server for you,
@@ -73,7 +76,73 @@ def url():
 #                               channel/leave                                  #
 #------------------------------------------------------------------------------#
 
+def register_default_user(url, name_first, name_last):
+    email = f'{name_first.lower()}{name_last.lower()}@gmail.com'
+    data = {
+        'email': email,
+        'password': 'password',
+        'name_first': name_first,
+        'name_last': name_last
+    }
+    payload = requests.post(f'{url}/auth/register', json=dumps(data))
+    return payload.json()
+
 #?-------------------------- Input/Access Error Testing ----------------------?#
+
+def test_input_leave_channel_id(url):
+    """Testing when an invalid channel_id is used as a parameter
+    """
+    requests.delete(f'{url}/clear')
+    user = register_default_user(url, 'John', 'Smith')
+
+    data = {
+        'token': user['token']
+        'name': 'Group 1',
+        'is_public': True,
+    }
+
+    # channel_data = requests.post(f'{url}/channels/create', json=dumps(data))
+
+    data = {'token': user['token'], 'channel_id': -1}
+    payload = requests.post(f'{url}/channel/leave', json=dumps(data))
+    assert payload.status_code == InputError.code
+
+    data = {'token': user['token'], 'channel_id': 0}
+    payload = requests.post(f'{url}/channel/leave', json=dumps(data))
+    assert payload.status_code == InputError.code
+
+    data = {'token': user['token'], 'channel_id': 1}
+    payload = requests.post(f'{url}/channel/leave', json=dumps(data))
+    assert payload.status_code == InputError.code
+
+    requests.delete(f'{url}/clear')
+
+def test_access_leave_user_is_member():
+    """Testing if a user was not in the channel initially
+    """
+    clear()
+    user_1 = auth.auth_register('johnsmith@gmail.com', 'password', 'John', 'Smith')
+    user_2 = auth.auth_register('janesmith@gmail.com', 'password', 'Jane', 'Smith')
+    new_channel_1 = channels.channels_create(user_1['token'], 'Group 1', True)
+    new_channel_2 = channels.channels_create(user_2['token'], 'Group 2', True)
+
+    with pytest.raises(AccessError):
+        channel.channel_leave(user_1['token'], new_channel_2['channel_id'])
+    with pytest.raises(AccessError):
+        channel.channel_leave(user_2['token'], new_channel_1['channel_id'])
+    clear()
+
+def test_access_leave_valid_token():
+    """Testing if token is valid
+    """
+    clear()
+    user = auth.auth_register('johnsmith@gmail.com', 'password', 'John', 'Smith')
+    new_channel = channels.channels_create(user['token'], 'Group 1', True)
+    auth.auth_logout(user['token'])
+
+    with pytest.raises(AccessError):
+        channel.channel_leave(user['token'], new_channel['channel_id'])
+    clear()
 
 
 #?------------------------------ Output Testing ------------------------------?#
