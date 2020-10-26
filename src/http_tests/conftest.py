6a2ebb94a9aa7d3afa11d5a797ea4a86ae643fa5
Gabriel Ting <65492346+gtangelo@users.noreply.github.com>
2020-10-26 11:24:04 +1100

    trying fix url fixture

--- src/http_tests/conftest.py
+++ src/http_tests/conftest.py
@@ -6,7 +6,34 @@ Pytest fixtures to use in testing files.
 import requests
 import pytest
 
+import re
+from subprocess import Popen, PIPE
+import signal
+from time import sleep
+
 from src.http_tests.helpers import register_default_user
+
+# Use this fixture to get the URL of the server. It starts the server for you,
+# so you don't need to.
+@pytest.fixture
+def url():
+    url_re = re.compile(r' \* Running on ([^ ]*)')
+    server = Popen(["python3", "src/http_tests/server.py"], stderr=PIPE, stdout=PIPE)
+    line = server.stderr.readline()
+    local_url = url_re.match(line.decode())
+    if local_url:
+        yield local_url.group(1)
+        # Terminate the server
+        server.send_signal(signal.SIGINT)
+        waited = 0
+        while server.poll() is None and waited < 5:
+            sleep(0.1)
+            waited += 0.1
+        if server.poll() is None:
+            server.kill()
+    else:
+        server.kill()
+        raise Exception("Couldn't get URL from local server")
 
 # User fixtures
 @pytest.fixture
