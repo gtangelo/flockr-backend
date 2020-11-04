b146c49bd1ac02398a5100f9f50158eba7e6b837
Tam Do <tdo713281@gmail.com>
2020-11-04 18:27:41 +1100

    Added the http tests

--- src/http_tests/user_http_test.py
+++ src/http_tests/user_http_test.py
@@ -9,6 +9,7 @@ Feature implementation was written by Christian Ilagan and Richard Quisumbing.
 import requests
 
 from src.feature.error import InputError, AccessError
+from src.feature.data import data
 
 #------------------------------------------------------------------------------#
 #                                 user/profile                                 #
@@ -35,7 +36,7 @@ def test_user_invalid(url, user_1):
     })
 
     assert profile_details.status_code == InputError.code
-
+    requests.delete(f'{url}/clear')
 
 #?------------------------------ Output Testing ------------------------------?#
 
@@ -48,6 +49,7 @@ def test_user_u_id(url, user_1):
     }).json()
 
     assert user_1['u_id'] == profile_details['user']['u_id']
+    requests.delete(f'{url}/clear')
 
 def test_valid_user_name(url, user_1):
     """Test whether the first and last name of a user is the same as the names returned in
@@ -60,6 +62,7 @@ def test_valid_user_name(url, user_1):
 
     assert profile_details['user']['name_first'] == 'John'
     assert profile_details['user']['name_last'] == 'Smith'
+    requests.delete(f'{url}/clear')
 
 def test_valid_user_email(url, user_1):
     """Test whether the user's email matches the email returned in user_profile.
@@ -70,6 +73,7 @@ def test_valid_user_email(url, user_1):
     }).json()
 
     assert profile_details['user']['email'] == 'johnsmith@gmail.com'
+    requests.delete(f'{url}/clear')
 
 def test_valid_user_handle(url, user_1):
     """Test whether the user's handle string matches the handle string returned in
@@ -90,7 +94,7 @@ def test_valid_user_handle(url, user_1):
             handle_str = account['handle_str']
 
     assert handle_str == profile_details['user']['handle_str']
-
+    requests.delete(f'{url}/clear')
 
 #------------------------------------------------------------------------------#
 #                             user/profile/setname                             #
@@ -136,6 +140,7 @@ def test_update_max_name(url, user_1):
         if user['u_id'] == user_1['u_id']:
             assert user['name_first'] == 'c'*50
             assert user['name_last'] == 'c'*50
+    requests.delete(f'{url}/clear')
     
 def test_update_min_name(url, user_1):
     """ Testing the basic functionality of maximum length names
@@ -176,7 +181,7 @@ def test_update_min_name(url, user_1):
         if user['u_id'] == user_1['u_id']:
             assert user['name_first'] == 'c'
             assert user['name_last'] == 'c'
-
+    requests.delete(f'{url}/clear')
     
 def test_update_invalid_token(url, user_1, logout_user_1):
     """ Testing using invalid tokens
@@ -188,6 +193,7 @@ def test_update_invalid_token(url, user_1, logout_user_1):
     }
     result = requests.put(f"{url}/user/profile/setname", json = data)
     assert result.status_code == InputError.code
+    requests.delete(f'{url}/clear')
 
 def test_invalid_chars(url, user_1):
     """ Testing using invalid characters for a name
@@ -206,6 +212,7 @@ def test_invalid_chars(url, user_1):
     }
     result = requests.put(f"{url}/user/profile/setname", json = data)
     assert result.status_code == InputError.code
+    requests.delete(f'{url}/clear')
 
 #?------------------------------ Output Testing ------------------------------?#
 
@@ -224,6 +231,7 @@ def test_update_names(url, user_1):
         if user['u_id'] == user_1['u_id']:
             assert user['name_first'] == 'Bobby'
             assert user['name_last'] == 'Michael'
+    requests.delete(f'{url}/clear')
 
 def test_update_name_first(url, user_1):
     """ Testing the basic functionality of changing only the first name
@@ -240,6 +248,7 @@ def test_update_name_first(url, user_1):
         if user['u_id'] == user_1['u_id']:
             assert user['name_first'] == 'Michael'
             assert user['name_last'] == 'Ilagan'
+    requests.delete(f'{url}/clear')
 
 def test_update_name_last(url, user_1):
     """ Testing the basic functionality of changing only the last name
@@ -256,6 +265,7 @@ def test_update_name_last(url, user_1):
         if user['u_id'] == user_1['u_id']:
             assert user['name_first'] == 'Christian'
             assert user['name_last'] == 'Michael'
+    requests.delete(f'{url}/clear')
 
 def test_update_consecutively(url, user_1):
     """ Testing the basic functionality constantly changing names
@@ -296,6 +306,7 @@ def test_update_consecutively(url, user_1):
         if user['u_id'] == user_1['u_id']:
             assert user['name_first'] == 'Harry'
             assert user['name_last'] == 'John'
+    requests.delete(f'{url}/clear')
     
 def test_update_multiple_users(url, user_1, user_2, user_3):
     """ Testing updating multiple users
@@ -336,6 +347,7 @@ def test_update_multiple_users(url, user_1, user_2, user_3):
         if user['u_id'] == user_3['u_id']:
             assert user['name_first'] == 'Chriss'
             assert user['name_last'] == 'Smoothie'
+    requests.delete(f'{url}/clear')
 
 #------------------------------------------------------------------------------#
 #                             user/profile/setemail                            #
@@ -352,6 +364,7 @@ def test_user_valid_setemail(url, user_1, logout_user_1):
     })
 
     assert ret_email.status_code == AccessError.code
+    requests.delete(f'{url}/clear')
 
 
 def test_email_already_exists(url, user_1, user_2):
@@ -363,6 +376,7 @@ def test_email_already_exists(url, user_1, user_2):
     })
     
     assert ret_email.status_code == InputError.code
+    requests.delete(f'{url}/clear')
 
 def test_update_same_email(url, user_1):
     """Setting the email that the user already has raises an error.
@@ -373,6 +387,7 @@ def test_update_same_email(url, user_1):
     })
     
     assert ret_email.status_code == InputError.code
+    requests.delete(f'{url}/clear')
 
 def test_ivalid_domain(url, user_1):
     """Test for no '@' character and missing string in the domain.
@@ -383,6 +398,7 @@ def test_ivalid_domain(url, user_1):
     })
     
     assert ret_email.status_code == InputError.code
+    requests.delete(f'{url}/clear')
 
 def test_no_period(url, user_1):
     """Test for no period '.' in the domain.
@@ -392,6 +408,7 @@ def test_no_period(url, user_1):
         'email': 'harry\\potter@outlookcom',
     })
     assert ret_email.status_code == InputError.code
+    requests.delete(f'{url}/clear')
 
 def test_capital_letter(url, user_1):
     """Setting a capital letter anywhere in the personal info part raises an 
@@ -403,6 +420,7 @@ def test_capital_letter(url, user_1):
     })
     
     assert ret_email.status_code == InputError.code
+    requests.delete(f'{url}/clear')
 
 def test_invalid_special_char(url, user_1):
     """Test for invalid characters (including special characters other than '\', '.' or '_').
@@ -413,6 +431,7 @@ def test_invalid_special_char(url, user_1):
     })
     
     assert ret_email.status_code == InputError.code
+    requests.delete(f'{url}/clear')
 
 def test_invalid_special_char_pos(url, user_1):
     """Test for characters '\', '.' or '_' at the end or start of the personal info part.
@@ -424,6 +443,7 @@ def test_invalid_special_char_pos(url, user_1):
     })
 
     assert ret_email.status_code == InputError.code
+    requests.delete(f'{url}/clear')
 
 #?------------------------------ Output Testing ------------------------------?#
 
@@ -441,6 +461,7 @@ def test_valid_email(url, user_1):
     }).json()
 
     assert 'harry_potter@bigpond.net' == profile_details['user']['email']
+    requests.delete(f'{url}/clear')
 
 def test_varying_domain(url, user_1):
     """Test for a domain other than @gmail.com
@@ -456,6 +477,7 @@ def test_varying_domain(url, user_1):
     }).json()
     
     assert 'harry.potter@company.co' == profile_details['user']['email']
+    requests.delete(f'{url}/clear')
 
 def test_update_email_four_times(url, user_1):
     """Test for multiple attempts at updating a user email.
@@ -481,6 +503,7 @@ def test_update_email_four_times(url, user_1):
     }).json()
     
     assert 'hogwarts@island.com' == profile_details['user']['email']
+    requests.delete(f'{url}/clear')
 
 def test_min_requirements(url, user_1):
     """Test for an email with very minimal requirements (2 letters in the personal
@@ -498,6 +521,7 @@ def test_min_requirements(url, user_1):
     }).json()
     
     assert 'ha@l.c' == profile_details['user']['email']
+    requests.delete(f'{url}/clear')
 
 #------------------------------------------------------------------------------#
 #                            user/profile/sethandle                            #
@@ -517,6 +541,7 @@ def test_handle_exists(url, user_1, user_2):
         'handle_str': 'valid_handle0',
     })
     assert result.status_code == InputError.code
+    requests.delete(f'{url}/clear')
 
 def test_handle_min(url, user_1):
     """ Testing that a user cannot change their handle below the min chars
@@ -541,6 +566,7 @@ def test_handle_min(url, user_1):
     })
     assert result_1.status_code == InputError.code
     assert result_2.status_code == InputError.code
+    requests.delete(f'{url}/clear')
 
 def test_handle_max(url, user_1):
     """ Testing that a user cannot change their handle above the max chars
@@ -565,6 +591,7 @@ def test_handle_max(url, user_1):
     })
     assert result_1.status_code == InputError.code
     assert result_2.status_code == InputError.code
+    requests.delete(f'{url}/clear')
 
 def test_update_handle_invalid_token(url, user_1, logout_user_1):
     """ Testing that an invalid token will not allow you to change the handle
@@ -574,6 +601,7 @@ def test_update_handle_invalid_token(url, user_1, logout_user_1):
         'handle_str': 'c'*20,
     })
     assert result_1.status_code == InputError.code
+    requests.delete(f'{url}/clear')
 
 def test_update_handle_same(url, user_1):
     """ Testing that a user cannot change their handle to their current handle
@@ -587,6 +615,7 @@ def test_update_handle_same(url, user_1):
         'handle_str': 'c'*20,
     })
     assert result_1.status_code == InputError.code
+    requests.delete(f'{url}/clear')
 
 def test_update_handle_chars(url, user_1):
     """ Testing invalid chars in handle
@@ -608,6 +637,7 @@ def test_update_handle_chars(url, user_1):
         'handle_str': 'Hel @l'*20,
     })
     assert result_1.status_code == InputError.code
+    requests.delete(f'{url}/clear')
 
 #?------------------------------ Output Testing ------------------------------?#
 
@@ -625,6 +655,7 @@ def test_handle_basic(url, user_1):
     }).json()
 
     assert profile_details['user']['handle_str'] == 'valid_handle'
+    requests.delete(f'{url}/clear')
 
 def test_handle_prefix(url, user_1, user_2, user_3):
     """ Testing the basic functionality of updating a handle
@@ -683,3 +714,209 @@ def test_handle_prefix(url, user_1, user_2, user_3):
     assert profile_details_1['user']['handle_str'] != profile_details_2['user']['handle_str']
     assert profile_details_2['user']['handle_str'] != profile_details_3['user']['handle_str']
     assert profile_details_1['user']['handle_str'] != profile_details_3['user']['handle_str']
+    requests.delete(f'{url}/clear')
+
+#------------------------------------------------------------------------------#
+#                          user_profile_uploadphoto                            #
+#------------------------------------------------------------------------------#
+
+#?------------------------ Input/Access Error Testing ------------------------?#
+
+def test_img_url_invalid_token(url, user_1, logout_user_1):
+    """Test for a non-registered/invalid user. (Invalid token)
+    """
+    img_url = "https://www.ottophoto.com/kirlian/kirlian_1/kirlian12.jpg"
+    result = requests.post(f"{url}/user/profile/uploadphoto", json={
+        'token': user_1['token'],
+        'img_url': img_url,
+        'x_start': 0,
+        'y_start': 0,
+        'x_end': 1,
+        'y_end': 1,
+    })
+    assert result.status_code == AccessError.code
+    requests.delete(f'{url}/clear')
+
+def test_img_url_status_not_200(url, user_1):
+    """ Test case where img_url returns a HTTP status other than 200.
+    """
+    x_start = 0
+    x_end = 1
+    y_start = 0
+    y_end = 1
+    result = requests.post(f"{url}/user/profile/uploadphoto", json={
+        'token': user_1['token'],
+        'img_url': 'https://fake_img',
+        'x_start': x_start,
+        'y_start': y_start,
+        'x_end': x_end,
+        'y_end': y_end,
+    })
+    assert result.status_code == InputError.code
+    result = requests.post(f"{url}/user/profile/uploadphoto", json={
+        'token': user_1['token'],
+        'img_url': 'https://',
+        'x_start': x_start,
+        'y_start': y_start,
+        'x_end': x_end,
+        'y_end': y_end,
+    })
+    assert result.status_code == InputError.code
+    requests.delete(f'{url}/clear')
+
+def test_img_url_xy_dimensions_not_valid(url, user_1):
+    """ Test case when any of x_start, y_start, x_end, y_end are not within the
+    dimensions of the image at the URL.
+    """
+    img_url = "https://www.ottophoto.com/kirlian/kirlian_1/kirlian12.jpg"
+    result = requests.post(f"{url}/user/profile/uploadphoto", json={
+        'token': user_1['token'],
+        'img_url': img_url,
+        'x_start': -1,
+        'y_start': -7,
+        'x_end': -1000,
+        'y_end': -777,
+    })
+    assert result.status_code == InputError.code
+    requests.delete(f'{url}/clear')
+
+def test_img_url_forbidden_access(url, user_1):
+    """ Test case where image uploaded cannot fetch its url due to a forbidden access
+    """
+    img_url = "http://pngimg.com/uploads/circle/circle_PNG62.png"
+    result = requests.post(f"{url}/user/profile/uploadphoto", json={
+        'token': user_1['token'],
+        'img_url': img_url,
+        'x_start': 0,
+        'y_start': 0,
+        'x_end': 1,
+        'y_end': 1,
+    })
+    assert result.status_code == InputError.code
+    requests.delete(f'{url}/clear')
+
+def test_img_url_not_jpg(url, user_1):
+    """ Test case where image uploaded is not a JPG
+    """
+    img_url = "https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png"
+    result = requests.post(f"{url}/user/profile/uploadphoto", json={
+        'token': user_1['token'],
+        'img_url': img_url,
+        'x_start': 0,
+        'y_start': 0,
+        'x_end': 1,
+        'y_end': 1,
+    })
+    assert result.status_code == InputError.code
+    requests.delete(f'{url}/clear')
+
+#?--------------------------- Output Testing ---------------------------------?#
+
+def test_img_url_normal_case(url, user_1):
+    """Test for a normal case where user uploads a jpg img
+    """
+    x_start = 0
+    x_end = 500
+    y_start = 0
+    y_end = 341
+    img_url = "https://www.ottophoto.com/kirlian/kirlian_1/kirlian12.jpg"
+    requests.post(f"{url}/user/profile/uploadphoto", json={
+        'token': user_1['token'],
+        'img_url': img_url,
+        'x_start': x_start,
+        'y_start': y_start,
+        'x_end': x_end,
+        'y_end': y_end,
+    })
+    user_profile = requests.get(f"{url}/user/profile", params={
+        'token': user_1['token'],
+        'u_id': user_1['u_id'],
+    }).json()
+    user_handle_1 = user_profile['user']
+    assert user_profile['user']['profile_img_url'] == f'static/{user_handle_1["handle_str"]}.jpg'
+    requests.delete(f'{url}/clear')
+
+def test_img_url_multiple_users_upload_and_change(url, user_1, user_2, user_3):
+    """Test for a when multiple users upload profile images and some change them.
+    """
+    x_start = 0
+    x_end = 500
+    y_start = 0
+    y_end = 341
+    img_url_1 = "https://www.ottophoto.com/kirlian/kirlian_1/kirlian12.jpg"
+    requests.post(f"{url}/user/profile/uploadphoto", json={
+        'token': user_1['token'],
+        'img_url': img_url_1,
+        'x_start': x_start,
+        'y_start': y_start,
+        'x_end': x_end,
+        'y_end': y_end,
+    })
+    user_profile_1 = user_profile = requests.get(f"{url}/user/profile", params={
+        'token': user_1['token'],
+        'u_id': user_1['u_id'],
+    }).json()
+    user_handle_1 = user_profile_1['user']
+    assert user_profile_1['user']['profile_img_url'] == f'static/{user_handle_1["handle_str"]}.jpg'
+
+    x_start = 0
+    x_end = 500
+    y_start = 0
+    y_end = 341
+    img_url_2 = "https://2017.brucon.org/images/b/bc/Twitter_logo.jpg"
+    requests.post(f"{url}/user/profile/uploadphoto", json={
+        'token': user_1['token'],
+        'img_url': img_url_2,
+        'x_start': x_start,
+        'y_start': y_start,
+        'x_end': x_end,
+        'y_end': y_end,
+    })
+
+    x_start = 0
+    x_end = 400
+    y_start = 0
+    y_end = 350
+    img_url_3 = "https://www.w3schools.com/w3css/img_nature.jpg"
+    requests.post(f"{url}/user/profile/uploadphoto", json={
+        'token': user_2['token'],
+        'img_url': img_url_3,
+        'x_start': x_start,
+        'y_start': y_start,
+        'x_end': x_end,
+        'y_end': y_end,
+    })
+
+    x_start = 500
+    x_end = 1500
+    y_start = 500
+    y_end = 1000
+    img_url_4 = "https://upload.wikimedia.org/wikipedia/commons/4/41/Sunflower_from_Silesia2.jpg"
+    requests.post(f"{url}/user/profile/uploadphoto", json={
+        'token': user_3['token'],
+        'img_url': img_url_4,
+        'x_start': x_start,
+        'y_start': y_start,
+        'x_end': x_end,
+        'y_end': y_end,
+    })
+
+    user_profile_1 = user_profile = requests.get(f"{url}/user/profile", params={
+        'token': user_1['token'],
+        'u_id': user_1['u_id'],
+    }).json()
+    user_profile_2 = user_profile = requests.get(f"{url}/user/profile", params={
+        'token': user_2['token'],
+        'u_id': user_2['u_id'],
+    }).json()
+    user_profile_3 = user_profile = requests.get(f"{url}/user/profile", params={
+        'token': user_3['token'],
+        'u_id': user_3['u_id'],
+    }).json()
+    user_handle_1 = user_profile_1['user']
+    user_handle_2 = user_profile_2['user']
+    user_handle_3 = user_profile_3['user']
+    assert user_profile_1['user']['profile_img_url'] == f'static/{user_handle_1["handle_str"]}.jpg'
+    assert user_profile_2['user']['profile_img_url'] == f'static/{user_handle_2["handle_str"]}.jpg'
+    assert user_profile_3['user']['profile_img_url'] == f'static/{user_handle_3["handle_str"]}.jpg'
+    requests.delete(f'{url}/clear')
